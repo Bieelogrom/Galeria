ebe80e4e167ddbbbe5c98fb64fdb9a8d1b707433
Bieelogrom <gabrielamorim15436@gmail.com>
2024-05-16 00:40:49 -0300

    first commit

---
+++ Galeria/src/estilo.jsx
@@ -0,0 +1,74 @@
+import styled, { css } from 'styled-components'
+
+const CorPrincipal = "#F26F03"
+const CorSecundaria = "#673C1C"
+const CorLetra = "#F5F5F5"
+
+// Navbar do site
+
+export const Nav = styled.nav`
+    width: 100%;
+    height: 100px;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    background: ${CorPrincipal};
+
+    display: flex;
+    justify-content: center;
+    gap: 50%;
+    align-items: center;
+
+
+
+    a{
+        color: ${CorLetra};
+    }
+`
+
+export const TituloSite = styled.div`
+    display: flex;
+    flex-direction: column;
+
+
+    small{
+        color: ${CorLetra};
+        text-align: center;
+    }
+
+    h1{
+        font-size: 32px;
+        font-weight: bold;
+        color: ${CorLetra};
+    }
+`
+
+export const OpcoesSite = styled.div`
+    display: flex;
+    gap: 10px;
+    
+
+    a{
+        font-size: 16px;
+        transition: color 1s;
+    }
+
+    a:hover{
+        font-size: 18px;
+        font-weight: bold;
+        color: ${CorSecundaria};
+        text-decoration: underline;
+        cursor: pointer;
+    }
+`
+
+// Fim Navbar
+
+// Inicio 
+
+export const MainWrapper = styled.main`
+    width: 100%;
+    height: 100%;
+    background: ${CorSecundaria};
+`
+
+// Fim inicio
---
+++ Galeria/src/routes/inicio.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+import { MainWrapper } from "../Estilo"
+
+function Inicio() {
+    return (
+        <>
+            <Navbar />
+            <MainWrapper>
+
+            </MainWrapper>
+        </>
+
+    )
+}
+
+export default Inicio
